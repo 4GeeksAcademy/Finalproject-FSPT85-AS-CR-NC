1decd8b377da4d70dc7e628042850707d9137fc8
CRVanware <crodriguez@vanware.io>
2025-02-14 20:35:57 +0000

    cards en home y footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,58 +1,51 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+    const primerostresVehiculos = store.vehicles.slice(0, 3);
 
 	return (
 		<div className="text-center">
 			<img src="https://www.enterprise.es/content/dam/ecom/utilitarian/emea/meet-the-fleet/one-enterprise-fleet-emea.jpg" className="img-fluid" alt="..." />
-        
-
-			<h2>Lista vehículos</h2>
-            {store.vehicles.length > 0 ? (
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Marca</th>
-                            <th>Modelo</th>
-                            <th>Potencia</th>
-                            <th>Plazas</th>
-                            <th>Combustible</th>
-                            <th>Autonomía</th>
-                            <th>Foto</th>
-                            <th>Año</th>
-                            <th>Precio por día</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {store.vehicles.map((vehicle) => (
-                            <tr key={vehicle.id}>
-                                <td>{vehicle.id}</td>
-                                <td>{vehicle.marca}</td>
-                                <td>{vehicle.modelo}</td>
-                                <td>{vehicle.potencia}</td>
-                                <td>{vehicle.plazas}</td>
-                                <td>{vehicle.combustible}</td>
-                                <td>{vehicle.autonomia}</td>
-                                <td>
-                                    <img
-                                        src={vehicle.foto}
-                                        alt={`${vehicle.marca} ${vehicle.modelo}`}
-                                        style={{ width: "50px", height: "auto" }}
-                                    />
-                                </td>
-                                <td>{vehicle.año}</td>
-                                <td>{vehicle.precio_por_dia.toFixed(2)} €</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            {primerostresVehiculos.length > 0 ? (
+                primerostresVehiculos.map((vehicle, index) => (
+                    <div
+                        key={index}
+                        className="card mb-3 mx-auto"
+                        style={{ maxWidth: "800px" }}
+                    >
+                        <div className="row no-gutters">
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">{`${vehicle.marca} ${vehicle.modelo}`}</h5>
+                                    <p className="card-text">
+                                        <strong>Potencia:</strong> {vehicle.potencia}<br />
+                                        <strong>Plazas:</strong> {vehicle.plazas}<br />
+                                        <strong>Combustible:</strong> {vehicle.combustible}<br />
+                                        <strong>Autonomía:</strong> {vehicle.autonomia}<br />
+                                        <strong>Año:</strong> {vehicle.año}<br />
+                                        <strong>Precio por día:</strong> {vehicle.precio_por_dia.toFixed(2)} €
+                                    </p>
+                                    <Link to={`/vehicle/${vehicle.id}`} className="btn btn-primary">
+                                        Ver detalles
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="col-md-4">
+                                <img
+                                    src={vehicle.foto}
+                                    alt={`${vehicle.marca} ${vehicle.modelo}`}
+                                    className="card-img"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))
             ) : (
-                <p>Loading vehicles...</p>
+                <p>Cargando vehículos...</p>
             )}
 		</div>
 	);
